815ebe1b07c9cea8c98adfa72840a6c92a2f5655
MikuInagaki <asterism.shootingstar@gmail.com>
2025-07-19 14:59:05 +0900

    掲示板一覧を作成

--- keijiban/src/App.jsx
+++ keijiban/src/App.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 
-
-export const App = () =>{
-  return(
-   <div>
-     <p>なーん</p>
-   </div>
-  )
+export const App = () => {
+ const[threads, setThreads] = useState([]);
  
-};
+ useEffect(()=>{
+  fetch('https://railway.bulletinboard.techtrain.dev/threads')
+  .then(res => res.json())
+  .then(data => {
+     console.log(data);
+     setThreads(data)})
+  
+
+  .catch(error => {
+  console.log('スレッド取得エラー:', error);
+ });
+
+},[]);
+
+return(
+   <div>
+      <h1 className="head">スレッド一覧</h1>
+      <ul className="thread-list">
+        {threads.map((thread) => (
+          <li key={thread.id} className="thread-card">
+            {thread.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
