c932daf73baef22bcbb4382de060a224f9a3c21d
Miku Inagaki <asterism.shootingstar@gmail.com>
2025-09-14 11:14:12 +0900

    NotFound.jsx消えていたのを復元

--- SSR-keijiban/src/App.server.jsx
+++ SSR-keijiban/src/App.server.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { ThreadList } from "./pages/ThreadList";
-import { Counter } from "./pages/Counter";
 
 export default function App() {
   return (
     <div className="container">
-      <Counter />
       <ThreadList />
     </div>
   );
