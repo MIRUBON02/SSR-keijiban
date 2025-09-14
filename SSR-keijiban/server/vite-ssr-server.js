c3cc6b2d3d7066754226d4bd3a98610335e69ebe
Miku Inagaki <asterism.shootingstar@gmail.com>
2025-09-14 14:43:10 +0900

    Station3 エラー改善0914

--- SSR-keijiban/server/vite-ssr-server.js
+++ SSR-keijiban/server/vite-ssr-server.js
@@ -1,11 +1,6 @@
 import express from "express";
 import { createServer as createViteServer } from "vite";
-import path from "path";
-import { fileURLToPath } from "url";
 import { renderToString } from "react-dom/server";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 async function createServer() {
   const app = express();
